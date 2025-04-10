6605c31a3effe88521bdf3302138cadf7740067e
idropthebeat <idropthebeat@gmail.com>
2025-04-10 15:17:11 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Wholesale Deal Finder</h1>
+      <p>Welcome to your real estate wholesale deal finder application!</p>
+    </div>
+  )
+}
