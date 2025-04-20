13687f48f75b6ad321282cf7c7f829c931cf862e
idropthebeat <idropthebeat@gmail.com>
2025-04-20 14:20:34 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
-// import { SessionProvider } from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
+import { Toaster } from 'react-hot-toast'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+      <Toaster position="top-right" />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
